05663002b37076a1498ea64ec839a0267c952493
Benson Lee <bensonlee5@gmail.com>
2025-07-27 23:39:07 -0700

    Bump version from 0.2.2 to 0.3.0

--- src/gridporter/__init__.py
+++ src/gridporter/__init__.py
@@ -1,6 +1,6 @@
 """GridPorter - Intelligent spreadsheet ingestion framework."""
 
-__version__ = "0.2.2"
+__version__ = "0.3.0"
 
 from gridporter.gridporter import GridPorter
 from gridporter.models import DetectionResult, TableInfo
